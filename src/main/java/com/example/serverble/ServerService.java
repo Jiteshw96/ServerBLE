c3374dca3da6c06b40fdac2604a73494bca0b479
jitesh <jitesh.wadhwani@atos.net>
2020-07-01 17:22:44 +0530

    "Permission Updated"

---
+++ src/main/java/com/example/serverble/ServerService.java
@@ -0,0 +1,248 @@
+package com.example.serverble;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import static com.example.serverble.ServerBleApplication.CHANNEL_ID;
+
+public class ServerService extends Service {
+    private List<BluetoothDevice> mDevices;
+    private BluetoothGattServer mGattServer;
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
+    ServerBleApplication serverBleApplication;
+    public static UUID SERVICE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+
+    //public static String CHARACTERISTIC_STRING = "18902a9a-1f4a-44fe-936f-14c8eea41801";
+    public static UUID CHARACTERISTIC_UUID = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+    private int REQUEST_ENABLE_BT = 1;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        mBluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mDevices = new ArrayList<>();
+
+        if (mBluetoothManager != null) {
+            mBluetoothAdapter = mBluetoothManager.getAdapter();
+        }
+
+
+        GattServerCallback gattServerCallback = new GattServerCallback(getApplicationContext());
+        mGattServer = mBluetoothManager.openGattServer(this, gattServerCallback);
+        setupServer();
+        startAdvertising();
+
+      String str = intent.getStringExtra("inputExtra");
+      Intent notificationIntent = new Intent(this,ServerActivity.class);
+      PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+      Notification notification = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setContentTitle("Server Service")
+                .setContentText(str)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1,notification);
+        //stopSelf();
+        //ContextCompat.startForegroundService(this,notification);
+
+
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void setupServer() {
+        /*BluetoothGattService service = new BluetoothGattService(SERVICE_UUID,
+                BluetoothGattService.SERVICE_TYPE_PRIMARY);
+        mGattServer.addService(service);*/
+        BluetoothGattService service1 = new BluetoothGattService(SERVICE_UUID,
+                BluetoothGattService.SERVICE_TYPE_PRIMARY);
+        BluetoothGattCharacteristic writeCharacteristic = new BluetoothGattCharacteristic(
+                CHARACTERISTIC_UUID,
+                BluetoothGattCharacteristic.PROPERTY_WRITE,
+                BluetoothGattCharacteristic.PERMISSION_WRITE);
+        service1.addCharacteristic(writeCharacteristic);
+
+        mGattServer.addService(service1);
+        mGattServer.getService(SERVICE_UUID);
+    }
+
+    private void stopServer() {
+        if (mGattServer != null) {
+
+            mGattServer.close();
+        }
+    }
+    private void restartServer() {
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 1);
+        startActivity(discoverableIntent);
+        stopServer();
+        setupServer();
+        startAdvertising();
+    }
+
+    private void startAdvertising() {
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 20);
+        startActivity(discoverableIntent);
+        Log.i("discoverable", "Is Discoverable");
+    }
+
+    private void notifyCharacteristic(byte[] value, UUID uuid) {
+        BluetoothGattService service = mGattServer.getService(SERVICE_UUID);
+        BluetoothGattCharacteristic characteristic = service.getCharacteristic(uuid);
+        Log.i("notify_char", "Notifying characteristic " + characteristic.getUuid().toString()
+                + ", new value: " +  com.example.serverble.StringUtils.byteArrayInHexFormat(value));
+
+        characteristic.setValue(value);
+        boolean confirm = com.example.serverble.BluetoothUtils.requiresConfirmation(characteristic);
+        for(BluetoothDevice device : mDevices) {
+            mGattServer.notifyCharacteristicChanged(device, characteristic, confirm);
+        }
+    }
+
+    public void sendResponse(BluetoothDevice device, int requestId, int status, int offset, byte[] value) {
+        mGattServer.sendResponse(device, requestId, status, 0, null);
+    }
+
+    private void sendReverseMessage(byte[] message) {
+        byte[] response =  ByteUtils.reverse(message);
+        serverBleApplication = (ServerBleApplication) getApplicationContext();
+        serverBleApplication.setClientMsg(com.example.serverble.StringUtils.stringFromBytes(message));
+        String str = serverBleApplication.getServerMsg();
+
+        Log.i("rev_send", "Sending: " +  com.example.serverble.StringUtils.byteArrayInHexFormat(response));
+        notifyCharacteristicEcho(com.example.serverble.StringUtils.bytesFromString(str));
+
+    }
+
+    public void notifyCharacteristicEcho(byte[] value) {
+        notifyCharacteristic(value, CHARACTERISTIC_UUID);
+    }
+
+
+
+    private class GattServerCallback extends BluetoothGattServerCallback {
+
+        private Context context;
+        GattServerCallback(Context context){
+
+            this.context = context;
+
+        }
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            super.onConnectionStateChange(device, status, newState);
+
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                addDevice(device);
+                Log.i("Connect", "Connected to "+device);
+                /*Intent intent = new Intent(ServerActivity.this, ChatActivity.class);
+                startActivity(intent);*/
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                removeDevice(device);
+            }
+        }
+
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device,
+                                                int requestId,
+                                                int offset,
+                                                BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicReadRequest(device, requestId, offset, characteristic);
+
+            Log.i("char_read", "onCharacteristicReadRequest "
+                    + characteristic.getUuid().toString());
+
+            if ( com.example.serverble.BluetoothUtils.requiresResponse(characteristic)) {
+                sendResponse(device, requestId, BluetoothGatt.GATT_FAILURE, 0, null);
+            }
+        }
+        @Override
+        public void onCharacteristicWriteRequest(BluetoothDevice device,
+                                                 int requestId,
+                                                 BluetoothGattCharacteristic characteristic,
+                                                 boolean preparedWrite,
+                                                 boolean responseNeeded,
+                                                 int offset,
+                                                 byte[] value) {
+            super.onCharacteristicWriteRequest(device,
+                    requestId,
+                    characteristic,
+                    preparedWrite,
+                    responseNeeded,
+                    offset,
+                    value);
+            Log.i("char_write", "onCharacteristicWriteRequest" + characteristic.getUuid().toString()
+                    + "\nReceived: " +  com.example.serverble.StringUtils.byteArrayInHexFormat(value) +" In String Format "+  com.example.serverble.StringUtils.stringFromBytes(value));
+
+
+            //    Toast.makeText(getApplicationContext(),  com.example.serverble.StringUtils.stringFromBytes(value),Toast.LENGTH_LONG).show();
+            if (CHARACTERISTIC_UUID.equals(characteristic.getUuid())) {
+                sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, null);
+                sendReverseMessage(value);
+            }
+        }
+
+        @Override
+        public void onNotificationSent(BluetoothDevice device, int status) {
+            super.onNotificationSent(device, status);
+            Log.i("send_notific", "onNotificationSent");
+        }
+    }
+
+    public void addDevice(BluetoothDevice device)
+    {
+        mDevices.add(device);
+    }
+
+    public void removeDevice(BluetoothDevice device)
+    {
+        mDevices.remove(device);
+    }
+
+
+}
